aced183f139cb093058f96a2f63c6054eea7feca
Francis Morissette <morissette.francis@gmail.com>
2017-05-30 20:41:57 -0400

    wip

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -21,8 +21,6 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        dd(auth()->user()->connectedAccounts->first()->getInstance()->refreshAccount());
-
         return view('dashboard.index');
     }
 }
